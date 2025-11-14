b732b3dc8fb45d8e43053b4d7b3f66829375635c
Eros Nicolino <erosnicolino@icloud.com>
2025-11-14 07:41:01 -0300

    fix: read JWT_SECRET from environment

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,16 @@
-export default ({ env }) => ({
+// backend/config/server.js
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['q2W8eR4tY1uI6oP7aS3dF5gH9jK0lLzX']),
+  },
+  admin: {
+    auth: {
+      secret: env('JWT_SECRET'), 
+    },
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
